a019ddc5094508be1566c481942fbe3436f8507c
QuentinVilfeu <quentin.vilfeu.dev@gmail.com>
2022-11-17 14:57:23 +0100

    test

--- ajout_produit.php
+++ ajout_produit.php
@@ -1,3 +1,9 @@
 <?php 
 
-test
+/**
+ * 4 status de fichier sur git
+ *      - untracked => les fichiers créé mais non suivies par git
+ *      - tracked => fichier déjà suivi par git mais non envoyé vers le repository
+ *      - staged => les fichiers suivi, enregistré mais non envoyé vers le repository 
+ *      - commited => tous les fichiers envoyé et non modifié de mon projet
+ */
